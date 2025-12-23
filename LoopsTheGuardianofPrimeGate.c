1599fba33bce280c68cadad70f47f49d1b5a5fe9
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2025-12-23 23:57:28 +0530

    Prime Gate number classification Problem
    Implement a program to classify a number as Prime, Composite, or None based on its properties.

---
+++ LoopsTheGuardianofPrimeGate.c
@@ -0,0 +1,75 @@
+//PROBLEM
+
+/*In the ancient kingdom of Zabiria, there exists a magical gate known as the Prime Gate.
+
+The gate reacts differently depending on the nature of the number it receives:
+
+It opens when given a Prime number
+It remains closed when given a Composite number
+It stays inactive when given a number that is neither Prime nor Composite
+A young programmer Omkar has been chosen to guard the gate. Every day, the gate provides a natural number.
+Omkar must determine the correct nature of the number so the gate behaves accordingly.
+
+Your task is to help Omkar by writing a program that checks whether the given number is Prime, Composite, or Neither.
+
+Input Format
+
+A single line containing one natural number N.
+
+Constraints
+
+-10^9 ≤ 𝑁 ≤ 10^9
+Output Format
+
+Print Prime, if the number is prime
+Print Composite, if the number is composite
+Print None, if the number is neither prime nor composite
+Sample Input 0
+
+7
+Sample Output 0
+
+Prime
+Explanation 0
+
+7 is a positive number greater than 1 and has only two divisors (1 and 7), so it is Prime.
+
+Sample Input 1
+
+10
+Sample Output 1
+
+Composite
+Explanation 1
+
+10 has more than two divisors (1, 2, 5, 10), so it is Composite.
+
+Sample Input 2
+
+1
+Sample Output 2
+
+None
+Explanation 2
+
+1 is neither prime nor composite, so it is None.*/
+
+//SOLUTION
+
+long long N;
+    scanf("%lld", &N);
+
+    if (N <= 1) {
+        printf("None");
+        return 0;
+    }
+
+    for (long long i = 2; i * i <= N; i++) {
+        if (N % i == 0) {
+            printf("Composite");
+            return 0;
+        }
+    }
+
+    printf("Prime");
+    return 0;
